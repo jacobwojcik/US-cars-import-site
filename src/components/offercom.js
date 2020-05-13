c81294ccb6dd5426ebe5fe334c367d9070d4ab09
Jac00bv <jacobyv11@gmail.com>
2020-05-13 13:23:36 +0200

    opinions2

--- src/components/offercom.js
+++ src/components/offercom.js
@@ -14,38 +14,37 @@ export default function Offercom() {
 Z USA LUB KANADY?</h1>
                     <Col md={4}>
                     <FontAwesomeIcon icon={faSearchDollar}  size = '4x' className="iconOffer" />
-                        <h2 className="blueP">1.	WYBÓR MODELU</h2>
-                        <p className="lBlue">Skontaktuj się z nami– zadzwoń lub napisz. Dzięki naszemu doświadczeniu pomożemy znaleźć Twój samochód marzeń i zrealizować import aut z USA lub Kanady.</p>
+                        <h2 className="blueP">1.	WYBÓR ODPOWIEDNIEGO MODELU</h2>
+                        <p className="lBlue">Współpracę rozpoczynamy od wyszukania odpowiedniego. Następnie auto jest przez nas weryfikowane poprzez Raporty Carfax oraz Autocheck. Po sprawdzeniu historii klient otrzymuje od nas kosztorys sprowadzenia. </p>
                     </Col>
                     <Col md={4}>
                     <FontAwesomeIcon icon={faCalculator}  size = '4x' className="iconOffer" />
-                        <h2 className="blueP">2.	KALKULACJA KOSZTÓW</h2>
-                        <p className="lBlue">Dokonamy  kalkulacji kosztów samochodu wraz z uwzględnieniem całej logistyki oraz dostarczenia wprost do Ciebie.</p>
+                        <h2 className="blueP">2.	UMOWA ORAZ ZALICZKA</h2>
+                        <p className="lBlue">Po dokonaniu wyboru pojazdu, podpisujemy umowę która jest formą zabezpieczenia dla obu stron. Klient jest także zobligowany do wpłaty kaucji zwrotnej w wysokości 10% kwoty licytacji. W przypadku niepomyślnego zakończenia licytacji kaucja jest zwracana, bądź pozostaje na poczet kolejnych aukcji. </p>
                     </Col>
                     <Col md={4}>
                     <FontAwesomeIcon icon={faClipboardList}  size = '4x' className="iconOffer" />
-                        <h2 className="blueP">3.	ZAMÓWIENIE</h2>
-                        <p className="lBlue">Podpisanie umowy, na podstawie której będziemy mogli zrealizować zamówienie. W umowie określone są etapy płatności oraz forma akceptacji wybranego przez Ciebie samochodu, jeszcze przed zakupem.</p>
+                        <h2 className="blueP">3.	PŁATNOŚC ZA SAMOCHÓD</h2>
+                        <p className="lBlue">Po otrzymaniu informacji o wygranej aukcji, Klient jest zobowiązany dokonać pełnej opłaty za koszty związane z zakupem oraz transportem samochodu do Europy bezpośrednio na konto firmowe w Stanach Zjednoczonych w ciągu 2 dni roboczych. Jest to warunek konieczny, aby uniknąć dodatkowo naliczonych opłat.  </p>
                     </Col>
                 </Row>
                 <Row>
                 <Col md={4}>
-                <FontAwesomeIcon icon={faCheckSquare}  size = '4x' className="iconOffer" />
+                <FontAwesomeIcon icon={faAngleDoubleRight}  size = '4x' className="iconOffer" />
 
-                        <h2 className="blueP">4.	REALIZACJA ZAMÓWIENIA</h2>
-                        <p className="lBlue">Zakupimy samochód idealny dla ciebie na jednej z oficjalnych aukcji IAAI, Copart, Manheim. Zostanie on wylicytowany w najkorzystniejszej  cenie. </p>
+                        <h2 className="blueP">4.	TRANSPORT</h2>
+                        <p className="lBlue">Po dokonaniu płatności za samochód, możemy rozpocząć procedurę transportu auta do Europy. Zawsze dostarczamy auto do najbliższego portu, przez co minimalizujemy koszty transportu lądowego po USA. Po dotarciu do portu auto zostaje załadowane do kontenera i wyrusza w podróż do Europy. Klient otrzymuje od nas link, w którym może monitorować lokalizacje kontenera</p>
                     </Col>
                     <Col md={4}>
-                    <FontAwesomeIcon icon={faAngleDoubleRight}  size = '4x' className="iconOffer" />
-
-                        <h2 className="blueP">5.	ZREALIZOWANY ZAKUP</h2>
-                        <p className="lBlue">Po zakupie zaakceptowanego samochodu, przystępujemy do realizacji formalności oraz  logistyki związanej z dostawą samochodu do portu, z którego będzie wysłany do Polski, a następnie odprawą celną, formalnościami i dostawą do naszej siedziby.</p>
+                    <FontAwesomeIcon icon={faCheckSquare}  size = '4x' className="iconOffer" />
+                        <h2 className="blueP">5.	ODPRAWA CELNA</h2>
+                        <p className="lBlue">Po dotarciu pojazdu do portu w Bremerhaven organizujemy opłaty celno-skarbowe. Po zakończeniu procedury clenia klient płaci należności bezpośrednio do agencji celnej.  </p>
                     </Col>
                     <Col md={4}>
                     <FontAwesomeIcon icon={faCarAlt}  size = '4x' className="iconOffer" />
 
-                        <h2 className="blueP">6.	PRZYGOTOWANIE AUTA DO ODBIORU</h2>
-                        <p className="lBlue">Dokonamy wszystkich formalności celno-skarbowych, klient odbiera auto w pełni opłacone ze wszystkimi dokumentami, książkami serwisowymi-pozostaje tylko rejestracja.</p>
+                        <h2 className="blueP">6.	ODBIÓR SAMOCHODU</h2>
+                        <p className="lBlue">Po zapłacie należności, auto zostaje zwolnione. Wtedy organizujemy transport pojazdu pod wybrany przez niego adres. Na życzenie klienta możemy zająć się naprawą samochodu oraz wszelkimi formalnościami związanymi z tłumaczeniem dokumentów, akcyzą oraz przeglądem. Klientowi zostaje jedynie zarejestrować i odebrać swój nowy samochód z siedziby naszej firmy. </p>
                     </Col>
                 </Row>
             </Container>
