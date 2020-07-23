657e1304c658f0233ffece5bbaf0b1308878834a
Jac00bv <jacobyv11@gmail.com>
2020-07-23 23:18:52 +0200

    fix progressbar view on mobile

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,13 +1,7 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
+import favicon from "../images/favicon.ico"
 import { useStaticQuery, graphql } from "gatsby"
 
 function SEO({ description, lang, meta, title }) {
@@ -68,7 +62,9 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link rel="icon" href={favicon} />
+    </Helmet>
   )
 }
 
